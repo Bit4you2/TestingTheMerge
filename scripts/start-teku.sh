68514fd544538812b31918dcfc08bc9e31a0e33b
Bit4You2 <bit4you2@gmail.com>
2022-10-05 20:16:20 +0200

    modified

--- scripts/start-teku.sh
+++ scripts/start-teku.sh
@@ -2,16 +2,25 @@
 #
 # Starts a beacon node.
 
-exec /teku/build/install/teku/bin/teku \
-       --data-path "/opt/teku/data" \
-       --rest-api-enabled=true \
-       --rest-api-docs-enabled=true \
-       --network kiln \
-       --Xee-version kilnv2 \
+BIN="/opt/teku/bin/teku"                # image: consensys/teku
+BIN="/teku/build/install/teku/bin/teku" # custom build
+
+exec $BIN \
+       --data-path "/opt/teku/data"  \
+       --rest-api-enabled=true       \
+       --rest-api-docs-enabled=true  \
+       --network kiln                \
+       --Xee-version kilnv2          \
        --ee-endpoint http://10.5.0.7:8545 \
        --ee-jwt-secret-file "/tmp/jwt/jwtsecret" \
        --validators-proposer-default-fee-recipient "0xb1B9CCe8F0BCB9046A605E9612fB8D2A97Eea77a" \
-       --log-destination console \
+       --log-destination console     \
        --validator-keys /opt/teku/data:/opt/teku/data \
        --Xnetwork-total-terminal-difficulty-override=20000000000000 \
+       --rest-api-cors-origins="*"   \
+       --metrics-enabled             \
+       --metrics-host-allowlist="*"  \
+       --metrics-port=8008           \
+       --metrics-categories=BEACON,PROCESS,LIBP2P,JVM,NETWORK,PROCESS \
+       --rest-api-host-allowlist="*" \
        --p2p-discovery-bootnodes "enr:-Iq4QMCTfIMXnow27baRUb35Q8iiFHSIDBJh6hQM5Axohhf4b6Kr_cOCu0htQ5WvVqKvFgY28893DHAg8gnBAXsAVqmGAX53x8JggmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk,enr:-Iq4QMCTfIMXnow27baRUb35Q8iiFHSIDBJh6hQM5Axohhf4b6Kr_cOCu0htQ5WvVqKvFgY28893DHAg8gnBAXsAVqmGAX53x8JggmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk,enr:-KG4QFkPJUFWuONp5grM94OJvNht9wX6N36sA4wqucm6Z02ECWBQRmh6AzndaLVGYBHWre67mjK-E0uKt2CIbWrsZ_8DhGV0aDKQc6pfXHAAAHAyAAAAAAAAAIJpZIJ2NIJpcISl6LTmiXNlY3AyNTZrMaEDHlSNOgYrNWP8_l_WXqDMRvjv6gUAvHKizfqDDVc8feaDdGNwgiMog3VkcIIjKA,enr:-MK4QI-wkVW1PxL4ksUM4H_hMgTTwxKMzvvDMfoiwPBuRxcsGkrGPLo4Kho3Ri1DEtJG4B6pjXddbzA9iF2gVctxv42GAX9v5WG5h2F0dG5ldHOIAAAAAAAAAACEZXRoMpBzql9ccAAAcDIAAAAAAAAAgmlkgnY0gmlwhKRcjMiJc2VjcDI1NmsxoQK1fc46pmVHKq8HNYLkSVaUv4uK2UBsGgjjGWU6AAhAY4hzeW5jbmV0cwCDdGNwgiMog3VkcIIjKA"
